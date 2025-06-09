520399224de23038fe3559cf147d496e082c2c21
Samuel Garcia Pereira <samgarciapereira@gmail.com>
2025-06-09 02:17:20 -0300

    commit desfeito

--- js/horario.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const agora = new Date();
-    const hora = agora.getHours();
-
-    if (hora < 11 || hora >= 18) {
-        document.getElementById("fora-do-horario").style.display = "flex";
-    }
-});
