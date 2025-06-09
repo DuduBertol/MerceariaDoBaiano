0cd6889a57bcb62f7f7547e2a000a82d2694c438
Isabelly Araújo <isabelly.araujo0608@gmail.com>
2025-06-09 09:27:55 -0300

    Arrumei para o horário certo

--- js/horario.js
+++ js/horario.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const agora = new Date();
     const hora = agora.getHours();
 
-    if (hora < 11 || hora >= 18) {
+    if (hora < 8 || hora >= 18) {
         document.getElementById("fora-do-horario").style.display = "flex";
     }
 });
